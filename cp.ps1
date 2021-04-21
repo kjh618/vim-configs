3383a15cc7697cba6760d3882ba5b59df3c8e27a
Jihoon Kim <kjh618@kaist.ac.kr>
2021-04-21 14:37:49 +0900

    Add .ideavimrc

--- cp.ps1
+++ cp.ps1
@@ -1,3 +1,5 @@
 Copy-Item "init.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\" -Verbose
 Copy-Item "ginit.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\" -Verbose
 Copy-Item "verilog.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\after\ftplugin\" -Verbose
+
+Copy-Item ".ideavimrc" -Destination "C:\Users\kjh\" -Verbose
