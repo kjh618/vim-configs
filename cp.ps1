0270fd4064b3740537af8452ef0eafab9c2e87ae
Jihoon Kim <kjh618@kaist.ac.kr>
2021-05-03 17:21:35 +0900

    Add options to ideavimrc

--- cp.ps1
+++ cp.ps1
@@ -1,4 +1,5 @@
 Copy-Item "init.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\" -Verbose
+Copy-Item "coc.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\" -Verbose
 Copy-Item "ginit.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\" -Verbose
 Copy-Item "verilog.vim" -Destination "C:\Users\kjh\AppData\Local\nvim\after\ftplugin\" -Verbose
 
