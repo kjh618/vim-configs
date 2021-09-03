93f081b524d044b0363bfaf12a608a7d6789b3dc
Jihoon Kim <kjh618@kaist.ac.kr>
2021-09-03 19:40:11 +0900

    Add mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -3,11 +3,22 @@
 
 vim.g.mapleader = " "
 
+-- Future defaults
+vim.api.nvim_set_keymap('n', 'Y', 'y$', { noremap = true })
+
+-- Keep selection when indenting
+vim.api.nvim_set_keymap('x', '<', '<gv', { noremap = true })
+vim.api.nvim_set_keymap('x', '>', '>gv', { noremap = true })
+
 -- Window movements
 vim.api.nvim_set_keymap('n', '<C-h>', '<C-w>h', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-j>', '<C-w>j', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-k>', '<C-w>k', { noremap = true })
 vim.api.nvim_set_keymap('n', '<C-l>', '<C-w>l', { noremap = true })
 
--- Yank all to clipboard
--- vim.api.nvim_set_keymap('n', '<Leader>ya', '<Cmd>%y+<CR>', { noremap = true })
+-- Stop highlighting search
+vim.api.nvim_set_keymap('n', '<Leader>l', '<Cmd>nohlsearch<CR>', { noremap = true })
+
+-- Yank/put to clipboard
+vim.api.nvim_set_keymap('', '<Leader>y', '"+y', { noremap = true })
+vim.api.nvim_set_keymap('', '<Leader>p', '"+p', { noremap = true })
