390350ec30885d84684757e5b9d4d11d462c648e
Jihoon Kim <jihoon.kim@devsisters.com>
2022-05-25 19:54:00 +0900

    Add window movement mappings

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -1,2 +1,8 @@
 -- Leader
 vim.g.mapleader = ' '
+
+-- Window movements
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
