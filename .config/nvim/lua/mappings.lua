48af9c11c981bc05d4c599311d985ff9ef8a9092
Jihoon Kim <jihoon.kim@devsisters.com>
2022-05-25 20:41:27 +0900

    Add remap options to window movements

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -2,7 +2,8 @@
 vim.g.mapleader = ' '
 
 -- Window movements
-vim.keymap.set('n', '<C-h>', '<C-w>h')
-vim.keymap.set('n', '<C-j>', '<C-w>j')
-vim.keymap.set('n', '<C-k>', '<C-w>k')
-vim.keymap.set('n', '<C-l>', '<C-w>l')
+-- `remap = true` for VSCode Neovim plugin
+vim.keymap.set('n', '<C-h>', '<C-w>h', { remap = true })
+vim.keymap.set('n', '<C-j>', '<C-w>j', { remap = true })
+vim.keymap.set('n', '<C-k>', '<C-w>k', { remap = true })
+vim.keymap.set('n', '<C-l>', '<C-w>l', { remap = true })
